fb53d110f25a7cf0a3f7964958d94a8c38a9a2cc
Adrian Cieplinski <adrian.cieplinski@gds.ey.com>
2020-01-27 17:31:23 +0100

    OCP: AndSpecification covers filtering by 2 criteria

--- src/DesignPatterns/solid/ocp/Demo.java
+++ src/DesignPatterns/solid/ocp/Demo.java
@@ -17,5 +17,8 @@ public class Demo {
 		pf.filter(products, new ColorSpecification(Color.GREEN))
 			.forEach(p -> System.out.println("- " + p.getName() + " is green"));
 
+		System.out.println("Blue and large products: ");
+		pf.filter(products, new AndSpecification<>(new ColorSpecification(Color.BLUE), new SizeSpecification(Size.LARGE)))
+			.forEach(p -> System.out.println("- " + p.getName() + " is blue and large"));
 	}
 }
